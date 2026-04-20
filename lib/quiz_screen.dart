89f854a78e77b8da78ba29b5da3c1aa2624c91d7
Gloria Oteri <gloriaoteri@gmail.com>
2026-04-19 23:52:09 -0400

    Added a loadQuestions function

--- lib/quiz_screen.dart
+++ lib/quiz_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'api_service.dart';
 import 'question.dart';
 
 class QuizScreen extends StatefulWidget {
@@ -8,10 +9,29 @@ class QuizScreen extends StatefulWidget {
 }
 
 class _QuizScreenState extends State<QuizScreen> {
-  List<String> _questions;
+  final ApiService api = ApiService();
+  List<Question> _questions = [];
   final int _currentQuestionIndex = 0;
   int _score = 0;
   final bool _answered = false;
+
+  // Loads the 10 questions to UI
+  @override
+  void initState() {
+    super.initState();
+    loadQuestions();
+  }
+
+  void loadQuestions () async {
+    final questionData = await api.getQuestions();
+    try {
+      setState(() {
+        _questions = questionData;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
 
   void updateScore () {
     setState(() {
@@ -24,11 +44,10 @@ class _QuizScreenState extends State<QuizScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary
-      ),
       body: Center(
         child: Column(
+          children: [
+          ]
       
     )));
   }
